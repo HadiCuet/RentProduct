0e5a3dcb1127c2e3c3210b88d70bf9efcdb3cc44
Abdullah Al Hadi <hadi.cuet@gmail.com>
2022-10-21 21:17:15 +0600

    Added empty project with coredata
    Signed-off-by: Abdullah Al Hadi <hadi.cuet@gmail.com>

---
+++ RentProduct/RentProduct/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RentProduct
+//
+//  Created by Abdullah Al Hadi on 21/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
