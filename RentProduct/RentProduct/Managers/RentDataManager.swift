cb25b16fb575b8f0a65b3904d1d75eb965cde192
Abdullah Al Hadi <hadi.cuet@gmail.com>
2022-10-22 18:02:48 +0600

    Filter product for book and return
    Signed-off-by: Abdullah Al Hadi <hadi.cuet@gmail.com>

--- RentProduct/RentProduct/Managers/RentDataManager.swift
+++ RentProduct/RentProduct/Managers/RentDataManager.swift
@@ -30,4 +30,36 @@ struct RentDataManager {
         Log.info("Data filtered count - \(products.count)")
         return products
     }
+
+    func filterProductsForBook() -> [ProductElement] {
+        var products = fetchProducts()
+
+        //1. Filter out not available products
+        products = products.filter({ product in
+            return product.availability
+        })
+
+        //2. Filter out need to repair products
+        products = products.filter({ product in
+            return !product.needingRepair
+        })
+
+        //3. Filter out durability finished products
+        products = products.filter({ product in
+            return product.durability < product.maxDurability
+        })
+        Log.info("Data filtered for book - \(products.count)")
+        return products
+    }
+
+    func filterProductsForReturn() -> [ProductElement] {
+        var products = fetchProducts()
+
+        //Filter products that is not available
+        products = products.filter({ product in
+            return !product.availability
+        })
+        Log.info("Data filtered for return - \(products.count)")
+        return products
+    }
 }
